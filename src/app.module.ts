29136e5e4c95e5716a80c1a3f0ea80b5e0f22f14
Chetana <100503849+chetananpatil@users.noreply.github.com>
2024-01-04 14:53:32 +0530

    Delete src/app.module.ts

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-// src/app.module.ts
-import { Module } from '@nestjs/common';
-import { MealsController } from './meals.controller';
-
-@Module({
-  imports: [],
-  controllers: [MealsController],
-})
-export class AppModule {}
